4398ddc3f53974fe97adff3973d72d51a35db92a
Antonio Campos <tonyldo@gmail.com>
2020-05-21 17:01:15 -0300

    almost done..

--- ttlocktomqtt/src/ttlock_adapter.py
+++ ttlocktomqtt/src/ttlock_adapter.py
@@ -1,35 +1,42 @@
-
 import paho.mqtt.client as mqtt
 import time
 import json
 import threading
 import re
+import concurrent.futures
 from ttlockwrapper import TTLock
 
-
-
+BROKER = '192.168.31.241'
+PORT = 1883
 
 
 DELAY_BETWEEN_NEW_THREADS_CREATION = 60
 DELAY_BETWEEN_BATTERY_SENDS = 60
+LOCK_NAME  = "TTLOCK_{}_{}"
 
 
 class TTLockToMqttClient(mqtt.Client):
     def __init__(self,mqttClientId,ttlockClientId,ttlockToken):
         super().__init__(mqttClientId)
-        self.mqttClientId=mqttClientId
         self.ttlockClientId = ttlockClientId
         self.ttlockToken = ttlockToken
+        self.ttlock = TTLock(self.ttlockClientId,self.ttlockToken)
+        self.mqttClientId=mqttClientId
         self.connected_flag=False
-        self.run_flag=True
         self.on_connect = TTLockToMqttClient.cb_on_connect
         self.on_disconnect = TTLockToMqttClient.cb_on_disconnect
         self.on_message = TTLockToMqttClient.cb_on_message
+        self.on_publish = TTLockToMqttClient.cb_on_publish
         self.username_pw_set(BROKER_USER, password=BROKER_PASS)
     
     def sendMensage(self,topic, msg):
         print('Client {} sending mensage "{}" to topic "{}"'.format(self.mqttClientId,msg,topic))
         self.publish(topic,msg) 
+
+    @classmethod
+    def cb_on_publish(cls, client, userdata, message):
+        print('Last send message time: {}'.format(time.time()))
+        client.lastBatterySending = time.time()
 
     @classmethod
     def cb_on_message(cls, client, userdata, message):
@@ -39,21 +46,27 @@ class TTLockToMqttClient(mqtt.Client):
     @classmethod
     def cb_on_disconnect(cls, client, userdata, rc):
         client.connected_flag=False #set flag
-        print("Client {} disconnected!".format(clientId.mqttClientId))
+        print("Client {} disconnected!".format(client.mqttClientId))
 
     @classmethod
     def cb_on_connect(cls,client, userdata, flags, rc):
         if rc==0:
             client.connected_flag=True #set flag
-            print("Client {} connected OK!".format(clientId.mqttClientId))
+            print("Client {} connected OK!".format(client.mqttClientId))
             client.sendDiscoveryMsgs()
         else:
-            print("Client {} Bad connection Returned code= {}".format(clientId.mqttClientId,rc))    
-
+            print("Client {} Bad connection Returned code= {}".format(client.mqttClientId,rc))    
 
 class TTLockToMqttClientLock(TTLockToMqttClient):
 
-    def __init__(self,lockId,ttlockClientId,ttlockToken):
+    def __init__(self,lockId,gatewayId,ttlockClientId,ttlockToken):
+        self.lockName = LOCK_NAME.format(gatewayId,lockId)
+        self.DISCOVERY_BATTERY_LEVEL_SENSOR_TOPIC = 'test/sensor/ttlock/{}/config'.format(self.lockName)
+        self.BATTERY_LEVEL_SENSOR_TOPIC = 'ttlocktomqtt/{}/battery'.format(self.lockName)
+        self.DISCOVERY_BATTERY_LEVEL_SENSOR_PAYLOAD = '{{"device_class": "battery", "name": "{}", "state_topic": "{}", "unit_of_measurement": "%", "value_template": "{{ value_json.battery}}" }}'
+        self.BATTERY_LEVEL_PAYLOAD = '{{ "name": "{}", "battery": {}}}'
+
+
         mqttClientId="lOCK-{}-{}".format(str(lockId),str(int(time.time())))
         super().__init__(mqttClientId,ttlockClientId,ttlockToken)
         self.lockId = lockId
@@ -61,77 +74,65 @@ class TTLockToMqttClientLock(TTLockToMqttClient):
     
     def sendLockBatteryLevel(self):
         if time.time()-self.lastBatterySending>DELAY_BETWEEN_BATTERY_SENDS:
-            batteryLevel = TTLock(self.ttlockClientId,self.ttlockToken).lock_electric_quantity(self.lockId)
-            msg = BATTERY_LEVEL_PAYLOAD.format(self.lockId,batteryLevel)
-            self.sendMensage(BATTERY_LEVEL_SENSOR_TOPIC,msg) 
-            self.lastBatterySending = time.time()
-    
+            batteryLevel = self.ttlock.lock_electric_quantity(self.lockId)
+            msg = self.BATTERY_LEVEL_PAYLOAD.format(self.lockName,batteryLevel)
+            self.sendMensage(self.BATTERY_LEVEL_SENSOR_TOPIC,msg) 
+            
     def sendDiscoveryMsgs(self):
-        msg = DISCOVERY_PAYLOAD.format(self.lockId,BATTERY_LEVEL_SENSOR_TOPIC)
-        self.sendMensage(DISCOVERY_BATTERY_LEVEL_SENSOR_TOPIC,msg) 
+        msg = self.DISCOVERY_BATTERY_LEVEL_SENSOR_PAYLOAD.format(self.lockName,self.BATTERY_LEVEL_SENSOR_TOPIC)
+        self.sendMensage(self.DISCOVERY_BATTERY_LEVEL_SENSOR_TOPIC,msg) 
+    
 
-class GatewayTTLockToMqttClient(TTLockToMqttClient):
-    def __init__(self,gatewayId,ttlockClientId,ttlockToken):
-        mqttClientId="GATEWAY-{}-{}".format(str(gatewayId),str(int(time.time())))
-        super().__init__(mqttClientId,ttlockClientId,ttlockToken)
-        self.gatewayId = gatewayId
-
-def client_loop(ttlockToMqttClient,keepalive=DELAY_BETWEEN_NEW_THREADS_CREATION,loop_delay=1,run_forever=False):
-    """runs a loop that will auto reconnect and subscribe to topics
-    pass topics as a list of tuples. You can pass a function to be
-    called at set intervals determined by the loop_delay
-    """
+def client_loop(lockId,gatewayId,keepalive=DELAY_BETWEEN_NEW_THREADS_CREATION,loop_delay=1,run_forever=False):
+    ttlockToMqttClient = TTLockToMqttClientLock(lockId,gatewayId,TTLOCK_CLIENTID,TTLOCK_TOKEN)
+    print("Creating TTlock Mqtt Client for lockid: {}".format(ttlockToMqttClient.mqttClientId))
+    print("Starting client {} loop".format(ttlockToMqttClient.mqttClientId))
+    bad_connection=0
     ttlockToMqttClient.connect(BROKER,PORT,keepalive)
-
-    while ttlockToMqttClient.run_flag: #loop
+    while run_flag: #loop
         ttlockToMqttClient.loop(1.0)
         if ttlockToMqttClient.connected_flag:
             ttlockToMqttClient.sendLockBatteryLevel()
+        else:
+            if bad_connection>5 and not run_forever:
+                break
+            bad_connection+=1
+            time.sleep(10)
 
-    print("disconnecting from: {}".format(BROKER))
     if ttlockToMqttClient.connected_flag:
         ttlockToMqttClient.disconnect()
 
+    return ttlockToMqttClient
 
-def createConnections():
+
+def createClients():
     ttlock = TTLock(TTLOCK_CLIENTID,TTLOCK_TOKEN)
     for gateway in ttlock.get_gateway_generator():
         for lock in ttlock.get_locks_per_gateway_generator(gateway.get("gatewayId")):
-            if lock.get('lockId') in clients.keys():
-                print( 'Lock {} mqtt client already created...'.format(lock.get('lockId')))
+            lockName = LOCK_NAME.format(gateway.get("gatewayId"),lock.get('lockId'))
+            if lockName in client_futures.keys() and not client_futures.get(lockName).done():
+                print( 'Lock {} mqtt client already created...'.format(lockName))
                 return
-            print( 'Creating TTlock Mqtt Client for lockid: {}'.format(lock.get('lockId')))
-            client = TTLockToMqttClientLock(lock.get('lockId'),TTLOCK_CLIENTID,TTLOCK_TOKEN)
+           
+            client_futures[lockName] = executor.submit(client_loop,lock.get('lockId'),gateway.get("gatewayId"),60)
 
-            clients[lock.get('lockId')]=client
-            t = threading.Thread(target=client_loop,args=(client,60))
-            t.start()
 
-def removeDeadConnections():
-    for clientId,client in clients.items():
-        if time.time - client.lastBatterySending > DELAY_BETWEEN_BATTERY_SENDS*2:
-            print('Stopping dead threading for client: {}'.format(clientId))
-
-            clients.pop(client.get('lockId'))
-            if client.connected_flag:
-                client.connected_flag=False
-
-clients=dict()
+client_futures=dict()
+executor = concurrent.futures.ThreadPoolExecutor()
+run_flag=True
 
 print("Starting main loop...")
 try:
     while True:
-        removeDeadConnections()
-        createConnections()
+        createClients()
         print("Current threads: {}".format(threading.active_count()))
         time.sleep(DELAY_BETWEEN_NEW_THREADS_CREATION)
 except KeyboardInterrupt:
     print("Ending...")
-    for clientId,client in clients.items():
-        print('Stopping threading for client: {}'.format(clientId))
-        client.run_flag=False
+    run_flag=False
+    for lockName,future in client_futures.items():
+        print("{} thread is over!".format(future.result().lockName))
 
-time.sleep(10)
    
 
 
