9bed9ecafe5e39e62b6c4e311cb0a5609431fd2d
Antonio Campos <tonyldo@gmail.com>
2020-07-17 09:46:15 -0300

    more fixes..

--- ttlocktomqtt/src/ttlock_adapter.py
+++ ttlocktomqtt/src/ttlock_adapter.py
@@ -1,152 +1,173 @@
 import paho.mqtt.client as mqtt
 import time
-import json
 import threading
-import re
 import concurrent.futures
-from ttlockwrapper import TTLock
-
-
+import getopt
+import sys
+from ttlockwrapper import TTLock,TTlockAPIError, constants
 
 DELAY_BETWEEN_NEW_THREADS_CREATION = 60
 DELAY_BETWEEN_LOCK_PUBLISH_INFOS = 60
 
 
-
 class TTLockToMqttClient(mqtt.Client):
-    def __init__(self,id,ttlockClientId,ttlockToken):
-        mqttClientId="lOCK-{}-{}".format(str(id),str(int(time.time())))
+    def __init__(self, id, ttlock, broker, port, broker_user, broker_pass,keepalive):
+        mqttClientId = "lOCK-{}-{}".format(str(id), str(int(time.time())))
         super().__init__(mqttClientId)
-        self.ttlockClientId = ttlockClientId
-        self.ttlockToken = ttlockToken
-        self.ttlock = TTLock(self.ttlockClientId,self.ttlockToken)
-        self.mqttClientId=mqttClientId
-        self.connected_flag=False
+        self.ttlock = ttlock
+        self.mqttClientId = mqttClientId
+        self.connected_flag = False
         self.on_connect = TTLockToMqttClient.cb_on_connect
         self.on_disconnect = TTLockToMqttClient.cb_on_disconnect
         self.on_message = TTLockToMqttClient.cb_on_message
         self.lastPublishInfo = time.time()
-        self.username_pw_set(BROKER_USER, password=BROKER_PASS)
-    
-    def sendMensage(self,topic, msg):
-        print('Client {} sending mensage "{}" to topic "{}"'.format(self.mqttClientId,msg,topic))
-        self.publish(topic,msg) 
+        self.broker_host = broker
+        self.broker_port = port
+        self.keepalive_mqtt = keepalive
+        if broker_user and broker_pass:
+                self.username_pw_set(broker_user, password=broker_pass)
+
+    def sendMensage(self, topic, msg):
+        print('Client {} sending mensage "{}" to topic "{}"'.format(
+            self.mqttClientId, msg, topic))
+        self.publish(topic, msg)
+        
+    def mqttConnection(self):
+        self.connect(self.broker_host, self.broker_port, self.keepalive_mqtt)
 
     @classmethod
     def cb_on_message(cls, client, userdata, message):
         time.sleep(1)
-        print("Client {} message received: {}".format(client.mqttClientId,str(message.payload.decode("utf-8"))))
+        print("Client {} message received: {}".format(
+            client.mqttClientId, str(message.payload.decode("utf-8"))))
         client.handleMessage(message)
 
     @classmethod
     def cb_on_disconnect(cls, client, userdata, rc):
-        client.connected_flag=False #set flag
+        client.connected_flag = False  # set flag
         print("Client {} disconnected!".format(client.mqttClientId))
 
     @classmethod
-    def cb_on_connect(cls,client, userdata, flags, rc):
-        if rc==0:
-            client.connected_flag=True #set flag
+    def cb_on_connect(cls, client, userdata, flags, rc):
+        if rc == 0:
+            client.connected_flag = True  # set flag
             print("Client {} connected OK!".format(client.mqttClientId))
-            client.subscribe(client.COMMAND_TOPIC)
+            if client.COMMAND_TOPIC:
+                print("Client {} subscribe on command topic: {}".format(client.mqttClientId,client.COMMAND_TOPIC))
+                client.subscribe(client.COMMAND_TOPIC)
             client.sendDiscoveryMsgs()
         else:
-            print("Client {} Bad connection Returned code= {}".format(client.mqttClientId,rc))    
+            print("Client {} Bad connection Returned code= {}".format(
+                client.mqttClientId, rc))
+
 
 class TTLockToMqttClientLock(TTLockToMqttClient):
 
-    def __init__(self,lock,gateway,ttlockClientId,ttlockToken):
+    def __init__(self, lock, gateway, ttlock, broker, port, broker_user, broker_pass,keepalive):
         self.lock = lock
         self.gateway = gateway
-        self.DISCOVERY_LOCK_TOPIC = 'test/lock/ttlock/{}_lock/config'.format(self.getLockId())
-        self.DISCOVERY_SENSOR_TOPIC = 'test/sensor/ttlock/{}_state/config'.format(self.getLockId())
-        self.DISCOVERY_BINARY_SENSOR_TOPIC = 'test/binary_sensor/ttlock/{}_battery/config'.format(self.getLockId())
-        self.BATTERY_LEVEL_SENSOR_TOPIC = 'ttlocktomqtt/{}/battery'.format(self.getLockId())
+        self.DISCOVERY_LOCK_TOPIC = 'test/lock/ttlock/{}_lock/config'.format(
+            self.getLockId())
+        self.DISCOVERY_SENSOR_TOPIC = 'test/sensor/ttlock/{}_battery/config'.format(
+            self.getLockId())
+        self.DISCOVERY_BINARY_SENSOR_TOPIC = 'test/binary_sensor/ttlock/{}_state/config'.format(
+            self.getLockId())
+        self.BATTERY_LEVEL_SENSOR_TOPIC = 'ttlocktomqtt/{}/battery'.format(
+            self.getLockId())
         self.COMMAND_TOPIC = 'ttlocktomqtt/{}/command'.format(self.getLockId())
-        self.STATE_SENSOR_TOPIC = 'ttlocktomqtt/{}/state'.format(self.getLockId())
+        self.STATE_SENSOR_TOPIC = 'ttlocktomqtt/{}/state'.format(
+            self.getLockId())
         self.DISCOVERY_STATE_SENSOR_PAYLOAD = '{{"device_class": "lock", "name": "{}", "state_topic": "{}", "value_template": "{{ value_json.lock}}", "uniq_id":"{}_state","device":{{"identifiers":["{}"],"connections":[["mac","{}"]]}}, "via_device":"{}" }}'
-        self.DISCOVERY_LOCK_PAYLOAD = '{{"name": "{}", "command_topic": "{}", "state_topic": "{}", "value_template": "{{ value_json.command}}", "state_locked":"off", "state_unlocked":"on" , "uniq_id":"{}_lock","device":{{"identifiers":["{}"],"connections":[["mac","{}"]]}}, "via_device":"{}" }}'
+        self.DISCOVERY_LOCK_PAYLOAD = '{{"name": "{}", "command_topic": "{}", "state_topic": "{}", "value_template": "{{ value_json.command}}", "uniq_id":"{}_lock","device":{{"identifiers":["{}"],"connections":[["mac","{}"]]}}, "via_device":"{}" }}'
         self.DISCOVERY_BATTERY_LEVEL_SENSOR_PAYLOAD = '{{"device_class": "battery", "name": "{}", "state_topic": "{}", "unit_of_measurement": "%", "value_template": "{{ value_json.battery}}", "uniq_id":"{}_battery","device":{{"identifiers":["{}"],"connections":[["mac","{}"]]}}, "via_device":"{}" }}'
         self.STATE_PAYLOAD = '{{"lock": "{}"}}'
-        self.COMMAND_PAYLOAD = '{{"command": "{}"}}'
         self.BATTERY_LEVEL_PAYLOAD = '{{"battery": {}}}'
-        
-        super().__init__(self.getLockId(),ttlockClientId,ttlockToken)
+
+        super().__init__(self.getLockId(), ttlock, broker, port, broker_user, broker_pass,keepalive)
 
     def getName(self):
-        return self.lock.get('lockAlias')
+        return self.lock.get(constants.LOCK_ALIAS_FIELD)
 
     def getLockId(self):
-        return self.lock.get('lockId')
-    
+        return self.lock.get(constants.LOCK_ID_FIELD)
+
     def getMac(self):
-        return self.lock.get('lockMac')
+        return self.lock.get(constants.LOCK_MAC_FIELD)
 
     def getGatewayId(self):
-        return self.gateway.get('gatewayId')
-    
-    def handleMessage(self,message):
+        return self.gateway.get(constants.GATEWAY_ID_FIELD)
+
+    def handleMessage(self, message):
         result = False
         command = str(message.payload.decode("utf-8"))
-        if command=="LOCK":
-            result = self.ttlock.lock()
-        elif command=="UNLOCK":
-            result = self.ttlock.unlock()
+        if command == 'lock':
+            result = self.ttlock.lock(self.getLockId())
+        elif command == 'unlock':
+            result = self.ttlock.unlock(self.getLockId())
         else:
             print('Invalid command.')
             return
         if not result:
-            #todo: send unavaible msg
+            # todo: send unavailble msg
             return
-        self.sendLockState()    
-
+        self.publishInfos()
 
     def publishInfos(self):
-        print("Time elapsed since last info send for {} lock: {}".format(self.getLockId(),time.time()-self.lastPublishInfo))
-        if time.time()-self.lastPublishInfo>DELAY_BETWEEN_LOCK_PUBLISH_INFOS:
-            self.sendLockBatteryLevel()
+        #print("Time elapsed since last info send for {} lock: {}".format(self.getLockId(), time.time()-self.lastPublishInfo))
+        if time.time()-self.lastPublishInfo > DELAY_BETWEEN_LOCK_PUBLISH_INFOS*3:
+            self.forcePublishInfos()
+
+    def forcePublishInfos(self):
+        try:
+            print('Publish mqtt lock {} information.'.format(self.getLockId()))
             self.sendLockState()
-            self.lastPublishInfo = time.time()
-        
-    
+            self.sendLockBatteryLevel()
+        except Exception as error:
+            print('While publish mqtt lock {} information: {}'.format(self.getLockId(),error))
+        self.lastPublishInfo = time.time()
+
+
     def sendLockBatteryLevel(self):
         batteryLevel = self.ttlock.lock_electric_quantity(self.getLockId())
         msg = self.BATTERY_LEVEL_PAYLOAD.format(batteryLevel)
-        self.sendMensage(self.BATTERY_LEVEL_SENSOR_TOPIC,msg) 
-    
+        self.sendMensage(self.BATTERY_LEVEL_SENSOR_TOPIC, msg)
+
     def sendLockState(self):
-        #Open state of lock:0-locked,1-unlocked,2-unknown
+        # Open state of lock:0-locked,1-unlocked,2-unknown
         state = self.ttlock.lock_state(self.getLockId())
         if state == 2:
-            #todo: send unavaible msg
-            pass
-        lock_is = "on" if state else "off"
+            print('While send {} lock state TTlockAPI return "unknown".'.format(self.getLockId()))
+            return
+        lock_is = 'unlocked' if state else 'locked'
         msg = self.STATE_PAYLOAD.format(lock_is)
-        self.sendMensage(self.STATE_SENSOR_TOPIC,msg) 
-            
+        self.sendMensage(self.STATE_SENSOR_TOPIC, msg)
+
     def sendDiscoveryMsgs(self):
-        print('Sending discoveries msgs...')
-        msg = self.DISCOVERY_BATTERY_LEVEL_SENSOR_PAYLOAD.format(self.getName(),self.BATTERY_LEVEL_SENSOR_TOPIC,self.getLockId(),self.getLockId(),self.getMac(),self.getGatewayId())
-        self.sendMensage(self.DISCOVERY_SENSOR_TOPIC,msg) 
-        msg = self.DISCOVERY_STATE_SENSOR_PAYLOAD.format(self.getName(),self.STATE_SENSOR_TOPIC,self.getLockId(),self.getLockId(),self.getMac(),self.getGatewayId())
-        self.sendMensage(self.DISCOVERY_BINARY_SENSOR_TOPIC,msg) 
-        msg = self.DISCOVERY_LOCK_PAYLOAD.format(self.getName(),self.COMMAND_TOPIC,self.STATE_SENSOR_TOPIC,self.getLockId(),self.getLockId(),self.getMac(),self.getGatewayId())
-        self.sendMensage(self.DISCOVERY_LOCK_TOPIC,msg) 
-
-
-def client_loop(lock,gateway,keepalive=DELAY_BETWEEN_NEW_THREADS_CREATION,loop_delay=1,run_forever=False):
-    ttlockToMqttClient = TTLockToMqttClientLock(lock,gateway,TTLOCK_CLIENTID,TTLOCK_TOKEN)
-    print("Created TTlock Mqtt Client for lockid: {}".format(ttlockToMqttClient.mqttClientId))
-    bad_connection=0
-    ttlockToMqttClient.connect(BROKER,PORT,keepalive)
-    while run_flag: #loop
-        ttlockToMqttClient.loop(1.0)
+        print('Sending discoveries msgs for client {}.'.format(self.mqttClientId))
+        msg = self.DISCOVERY_BATTERY_LEVEL_SENSOR_PAYLOAD.format(self.getName(
+        ), self.BATTERY_LEVEL_SENSOR_TOPIC, self.getLockId(), self.getLockId(), self.getMac(), self.getGatewayId())
+        self.sendMensage(self.DISCOVERY_SENSOR_TOPIC, msg)
+        msg = self.DISCOVERY_STATE_SENSOR_PAYLOAD.format(self.getName(
+        ), self.STATE_SENSOR_TOPIC, self.getLockId(), self.getLockId(), self.getMac(), self.getGatewayId())
+        self.sendMensage(self.DISCOVERY_BINARY_SENSOR_TOPIC, msg)
+        msg = self.DISCOVERY_LOCK_PAYLOAD.format(self.getName(), self.COMMAND_TOPIC, self.STATE_SENSOR_TOPIC, self.getLockId(
+        ), self.getLockId(), self.getMac(), self.getGatewayId())
+        self.sendMensage(self.DISCOVERY_LOCK_TOPIC, msg)
+    
+def client_loop(lock, gateway, ttlock, broker, port, broker_user, broker_pass, keepalive, loop_delay=1.0, run_forever=False):
+    ttlockToMqttClient = TTLockToMqttClientLock(lock, gateway, ttlock, broker, port, broker_user, broker_pass,keepalive)
+    print("Created TTlock Mqtt Client for lockid: {}".format(
+        ttlockToMqttClient.mqttClientId))
+    bad_connection = 0
+    ttlockToMqttClient.mqttConnection()
+    while run_flag:  # loop
+        ttlockToMqttClient.loop(loop_delay)
         if ttlockToMqttClient.connected_flag:
             ttlockToMqttClient.publishInfos()
         else:
-            if bad_connection>5 and not run_forever:
+            if bad_connection > 5 and not run_forever:
                 break
-            bad_connection+=1
+            bad_connection += 1
             time.sleep(10)
 
     if ttlockToMqttClient.connected_flag:
@@ -155,36 +176,77 @@ def client_loop(lock,gateway,keepalive=DELAY_BETWEEN_NEW_THREADS_CREATION,loop_d
     return ttlockToMqttClient
 
 
-def createClients():
-    ttlock = TTLock(TTLOCK_CLIENTID,TTLOCK_TOKEN)
+def createClients(broker, port, broker_user, broker_pass, ttlock_client, ttlock_token):
+    ttlock = TTLock(ttlock_client, ttlock_token)
     for gateway in ttlock.get_gateway_generator():
-        for lock in ttlock.get_locks_per_gateway_generator(gateway.get("gatewayId")):
-            if lock.get('lockId') in client_futures.keys() and not client_futures.get(lock.get('lockId')).done():
-                print( 'Lock {} mqtt client already created...'.format(lock.get('lockId')))
-                return
-           
-            client_futures[lock.get('lockId')] = executor.submit(client_loop,lock,gateway,60)
+        for lock in ttlock.get_locks_per_gateway_generator(gateway.get(constants.GATEWAY_ID_FIELD)):
+            if lock.get(constants.LOCK_ID_FIELD) in client_futures.keys() and not client_futures.get(lock.get(constants.LOCK_ID_FIELD)).done():
+                print('Lock {} mqtt client already created...'.format(lock.get(constants.LOCK_ID_FIELD)))
+                
+            else:
+                client_futures[lock.get(constants.LOCK_ID_FIELD)] = executor.submit(client_loop, lock, gateway, ttlock, broker, port, broker_user, broker_pass, DELAY_BETWEEN_NEW_THREADS_CREATION)
+            time.sleep(DELAY_BETWEEN_NEW_THREADS_CREATION*2)
+            
 
 
-client_futures=dict()
+def main(broker, port, broker_user, broker_pass, ttlock_client, ttlock_token):
+    try:
+        if not ttlock_client or not ttlock_token:
+            raise ValueError('Invalid ttlock client or token.')
+        
+        print("Starting main loop...")
+        while True:
+            try:
+                createClients(broker, port, broker_user, broker_pass,
+                ttlock_client, ttlock_token)
+                print("Current threads: {}".format(threading.active_count()))
+            except Exception as exception:
+                print(exception)
+            time.sleep(DELAY_BETWEEN_NEW_THREADS_CREATION)
+
+    except KeyboardInterrupt :
+        print("Ending...")
+        global run_flag
+        run_flag = False
+        for lockId, future in client_futures.items():
+            print("{} thread is over!".format(future.result().getLockId()))
+    except ValueError as error:
+        print(error)
+
+
+run_flag = True
+client_futures = dict()
 executor = concurrent.futures.ThreadPoolExecutor()
-run_flag=True
 
-print("Starting main loop...")
-try:
-    while True:
-        createClients()
-        print("Current threads: {}".format(threading.active_count()))
-        time.sleep(DELAY_BETWEEN_NEW_THREADS_CREATION)
-except KeyboardInterrupt:
-    print("Ending...")
-    run_flag=False
-    for lockId,future in client_futures.items():
-        print("{} thread is over!".format(future.result().getLockId()))
+if __name__ == '__main__':
+    broker = 'localhost'
+    port = 1883
+    broker_user = None
+    broker_pass = None
+    ttlock_client = None
+    ttlock_token = None
+    full_cmd_arguments = sys.argv
+    argument_list = full_cmd_arguments[1:]
+    short_options = 'b:p:u:P:c:t:'
+    long_options = ['broker=', 'port=', 'user=', 'Pass=', 'client=', 'token=']
+    try:
+        arguments, values = getopt.getopt(argument_list, short_options, long_options)
+    except getopt.error as err:
+        print (str(err))
+        sys.exit(2)
 
-   
+    for current_argument, current_value in arguments:
+        if current_argument in ("-b", "--broker"):
+            broker = current_value
+        elif current_argument in ("-p", "--port"):
+            port = current_value
+        elif current_argument in ("-u", "--user"):
+            broker_user = current_value
+        elif current_argument in ("-P", "--Pass"):
+            broker_pass = current_value
+        elif current_argument in ("-c", "--client"):
+            ttlock_client = current_value
+        elif current_argument in ("-t", "--token"):
+            ttlock_token = current_value
 
-
-
-
-
+    main(broker, port, broker_user, broker_pass, ttlock_client, ttlock_token)
