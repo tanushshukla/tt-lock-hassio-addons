10fab45bae506ae7463700f55979658b156f5ed6
Antonio Campos <tonyldo@gmail.com>
2020-08-21 12:06:44 -0300

    fix parameters

--- ttlock2mqtt/run.sh
+++ ttlock2mqtt/run.sh
@@ -10,4 +10,4 @@ PUBLISH_STATE_DELAY=$(bashio::config 'publishstatedelay')
 PUBLISH_BATTERY_DELAY=$(bashio::config 'publishbatterydelay')
 LOG_LEVEL=$(bashio::config 'loglevel')
 
-exec python3 /ttlock_adapter.py --client=${TTLOCK_CLIENT_APP} --token=${TTLOCK_TOKEN} --broker=${MQTT_BROKER_HOST} --port=${MQTT_BROKER_PORT} --user=${MQTT_BROKER_USER} --Pass=${MQTT_BROKER_PASS} --State_delay=${PUBLISH_STATE_DELAY} --Battery_delay=${PUBLISH_BATTERY_DELAY} --log_level=${LOG_LEVEL}
+exec python3 /ttlock_adapter.py --client=${TTLOCK_CLIENT_APP} --token=${TTLOCK_TOKEN} --broker=${MQTT_BROKER_HOST} --port=${MQTT_BROKER_PORT} --user=${MQTT_BROKER_USER} --Pass=${MQTT_BROKER_PASS} -S=${PUBLISH_STATE_DELAY} -B=${PUBLISH_BATTERY_DELAY} --log_level=${LOG_LEVEL}
